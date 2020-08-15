60b9659b13d2cd0c29969b41d21516820e7d2c69
asadalibest1 <asadalibest1@gmail.com>
2020-08-15 00:25:27 -0700

    Updates

---
+++ precache-manifest.bc9b70762b1203b83580f9b3637302fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615d22d28c9409dbfd41816d0b5fde7d",
+    "url": "/red-queen-animation-app/index.html"
+  },
+  {
+    "revision": "16081d660899b9495403",
+    "url": "/red-queen-animation-app/static/css/main.57a7a4b1.chunk.css"
+  },
+  {
+    "revision": "896924337ebfe1f3ca6f",
+    "url": "/red-queen-animation-app/static/js/2.b0ce827d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/red-queen-animation-app/static/js/2.b0ce827d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16081d660899b9495403",
+    "url": "/red-queen-animation-app/static/js/main.09e1bfd0.chunk.js"
+  },
+  {
+    "revision": "eff088f3996167c28688",
+    "url": "/red-queen-animation-app/static/js/runtime-main.f825ec71.js"
+  }
+]);
